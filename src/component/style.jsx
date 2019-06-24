57cd2031236805a351e92ebf1917ddf761d592b6
gimtommang <sh20058@naver.com>
2019-06-24 23:32:39 +0900

    styled component 를 통한 스타일링 추가

--- src/component/style.jsx
+++ src/component/style.jsx
@@ -1,11 +1,42 @@
 import styled from "styled-components";
 
-export const Title = styled.div`
+export const Todo = styled.div`
+  display : flex;
+  flex-direction : column;
+  width : 1219px;
+  
+  border : 1px solid pink;
+`
 
+export const Title = styled.div`
+  text-align : center;
+  width : 1219px;
+  height : 97px;
+  background-color : #FFD9D1;
+  color : white;
+  font-size : 65px;
+  text-align : center;
 `
 
 export const Input = styled.div`
+  display : flex;
+  align-items : center;
+  width : 1219px;
+  height : 103px;
+`
 
+export const InputField = styled.input`
+  margin-left :32px ;
+  font-size :28px;
+  width : 1018px;
+  height : 65px;
+`
+
+export const InputBtn = styled.button`
+  width : 130px;
+  height  65px; 
+  background-color : #FFD9D1;
+  border : none;
 `
 
 export const List = styled.div`
@@ -14,21 +45,47 @@ export const List = styled.div`
 export const Check = styled.input`
 `
 export const Info = styled.div`
-  margin-bottom : 10px;
   display : flex;
-  background-color : ${props => this.props.isChecked ?
+  align-items : center; 
+  height :68px;
+  border : 1px solid pink;
+  background-color : ${props => props.isChecked ?
     "#E4E4E4":
     "#FFFf"
-  }
-  
+ }  
   &:hover{
     background-color : #E4E4E4;
   }
 `
+
+export const Contnet = styled.div`
+   font-size :28px;
+   text-decoration: ${props => props.isChecked ?
+      "line-through" :
+      "none"
+}
+    color : ${props => props.isChecked ?
+      "#707070" :
+      "#474747"
+}
+
+
+`
+
 export const DeleteIcon = styled.button`
+  align-items  : center;
+  margin-left : 21px;
+  margin-right : 30px;
+  width : 45px;
+  height : 45px;
   color : red; 
+  font-size : 30px;
+  background-color : #ffff;
+  border : 1px solid pink;
 `
  
 export const CheckedIcon = styled.div`
+font-size : 30px;
+  justify-content : flex-end; 
   color : green;
 `
